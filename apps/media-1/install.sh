ab0c6fba935455544e9515452f28883b9e175857
Alerant_HorvathG <gabor.horvath3@erstebank.hu>
2025-02-28 15:55:15 +0100

    Update Plex repository configuration in media-1 installation script

--- apps/media-1/install.sh
+++ apps/media-1/install.sh
@@ -38,11 +38,12 @@ step_start "Operating System" "Updating" "Updated"
   pkg_upgrade
 
 step_start "Dependencies" "Installing" "Installed"
-  pkg_add curl haveged gpg apt-transport-https apt-utils ca-certificates
+  pkg_add apt-utils ca-certificates
 
 step_start "Plex Repository" "Adding" "Added"
-  curl https://downloads.plex.tv/plex-keys/PlexSign.key | gpg --dearmor | sudo tee /usr/share/keyrings/plex-archive-keyring.gpg >/dev/null
-  echo deb [signed-by=/usr/share/keyrings/plex-archive-keyring.gpg] https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+  #curl https://downloads.plex.tv/plex-keys/PlexSign.key | gpg --dearmor | sudo tee /usr/share/keyrings/plex-archive-keyring.gpg >/dev/null
+  #echo deb [signed-by=/usr/share/keyrings/plex-archive-keyring.gpg] https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+  echo deb https://downloads.plex.tv/repo/deb/ public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
 
 step_start "Plex" "Installing" "Installed"
   pkg_update
