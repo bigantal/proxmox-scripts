4251c340de5c7638114ca9dbe33b65f8ead43c1a
Alerant_HorvathG <gabor.horvath3@erstebank.hu>
2025-03-19 12:01:34 +0100

    Add Jenkins installation script and configuration

---
+++ apps/jenkins/install.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+EPS_BASE_URL=${EPS_BASE_URL:-}
+EPS_OS_DISTRO=${EPS_OS_DISTRO:-}
+EPS_UTILS_COMMON=${EPS_UTILS_COMMON:-}
+EPS_UTILS_DISTRO=${EPS_UTILS_DISTRO:-}
+EPS_APP_CONFIG=${EPS_APP_CONFIG:-}
+EPS_CLEANUP=${EPS_CLEANUP:-false}
+EPS_CT_INSTALL=${EPS_CT_INSTALL:-false}
+
+if [ -z "$EPS_BASE_URL" -o -z "$EPS_OS_DISTRO" -o -z "$EPS_UTILS_COMMON" -o -z "$EPS_UTILS_DISTRO" -o -z "$EPS_APP_CONFIG" ]; then
+  printf "Script loaded incorrectly!\n\n";
+  exit 1;
+fi
+
+source <(echo -n "$EPS_UTILS_COMMON")
+source <(echo -n "$EPS_UTILS_DISTRO")
+source <(echo -n "$EPS_APP_CONFIG")
+
+pms_bootstrap
+pms_settraps
+
+if [ $EPS_CT_INSTALL = false ]; then
+  pms_header
+  pms_check_os
+fi
+
+EPS_OS_ARCH=$(os_arch)
+EPS_OS_CODENAME=$(os_codename)
+EPS_OS_VERSION=${EPS_OS_VERSION:-$(os_version)}
+
+################################################################
+##   Source: https://www.jenkins.io/doc/book/installing/linux/#debianubuntu
+################################################################
+
+step_start "Operating System" "Updating" "Updated"
+  pkg_update
+#  pkg_upgrade
+
+#step_start "Dependencies" "Installing" "Installed"
+#  pkg_add curl haveged gpg openjdk-17-jre-headless
+
+
+step_start "Jenkins Repository" "Adding" "Added"
+  sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+  echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+step_start "Jenkins" "Installing" "Installed"
+  pkg_update
+  pkg_add jenkins
+  svc_add jenkins
+  svc_start jenkins
+
+step_start "Environment" "Cleaning" "Cleaned"
+  if [ "$EPS_CLEANUP" = true ]; then
+    pkg_del "$EPS_DEPENDENCIES"
+  fi
+  pkg_clean
+
+step_end "Installation complete"
+
+printf "\nThe application should be reachable at ${CLR_CYB}https://$(os_ip):8443${CLR}\n\n"
